065e62c44aee0bbab755100599ade3292b1c4a4d
Matias <matias.kovero@gmail.com>
2020-02-11 19:34:15 +0200

    Updated docs

--- docs/scripts/search.js
+++ docs/scripts/search.js
@@ -1,83 +1,62 @@
 
-var searchAttr = 'data-search-mode';
-function contains(a,m){
-    return (a.textContent || a.innerText || "").toUpperCase().indexOf(m) !== -1;
-};
+function hideSearchList(){
+  document.getElementById("search-item-ul").style.display = "none";
+}
 
-//on search
-document.getElementById("nav-search").addEventListener("keyup", function(event) {
-    var search = this.value.toUpperCase();
+function showSearchList(){  
+  document.getElementById("search-item-ul").style.display = "block";
+}
 
-    if (!search) {
-        //no search, show all results
-        document.documentElement.removeAttribute(searchAttr);
-        
-        document.querySelectorAll("nav > ul > li:not(.level-hide)").forEach(function(elem) {
-            elem.style.display = "block";
-        });
+function checkClick(e){
+  if( e.target.id != "search-box"){
+    this.setTimeout(function(){
+      hideSearchList()
+    }, 60);
+    
+    window.removeEventListener('click', checkClick)
+  }
+}
 
-        if (typeof hideAllButCurrent === "function"){
-            //let's do what ever collapse wants to do
-            hideAllButCurrent();
-        } else {
-            //menu by default should be opened
-            document.querySelectorAll("nav > ul > li > ul li").forEach(function(elem) {
-                elem.style.display = "block";
-            });
-        }
-    } else {
-        //we are searching
-        document.documentElement.setAttribute(searchAttr, '');
+function setupSearch(){
+  var input_box = document.getElementById("search-box");
+  var keys = ["title"]
 
-        //show all parents
-        document.querySelectorAll("nav > ul > li").forEach(function(elem) {
-            elem.style.display = "block";
-        });
-        //hide all results
-        document.querySelectorAll("nav > ul > li > ul li").forEach(function(elem) {
-            elem.style.display = "none";
-        });
-        //show results matching filter
-        document.querySelectorAll("nav > ul > li > ul a").forEach(function(elem) {
-            if (!contains(elem.parentNode, search)) {
-                return;
-            }
-            elem.parentNode.style.display = "block";
-        });
-        //hide parents without children
-        document.querySelectorAll("nav > ul > li").forEach(function(parent) {
-            var countSearchA = 0;
-            parent.querySelectorAll("a").forEach(function(elem) {
-                if (contains(elem, search)) {
-                    countSearchA++;
-                }
-            });
-            
-            var countUl = 0;
-            var countUlVisible = 0;
-            parent.querySelectorAll("ul").forEach(function(ulP) {
-                // count all elements that match the search
-                if (contains(ulP, search)) {
-                    countUl++;
-                }
-                
-                // count all visible elements
-                var children = ulP.children
-                for (i=0; i<children.length; i++) {
-                    var elem = children[i];
-                    if (elem.style.display != "none") {
-                        countUlVisible++;
-                    }
-                }
-            });
-          
-            if (countSearchA == 0 && countUl === 0){
-                //has no child at all and does not contain text
-                parent.style.display = "none";
-            } else if(countSearchA == 0 && countUlVisible == 0){
-                //has no visible child and does not contain text
-                parent.style.display = "none";
-            }
-        });
+  input_box.addEventListener('keyup', function(){
+    if(input_box.value != ""){ 
+      showSearchList();
+      search(list, keys, input_box.value)
     }
-});
+    else hideSearchList();
+  })
+
+  input_box.addEventListener('focus', function(){
+    showSearchList();
+    if(input_box.value != "") search(list, keys, input_box.value)
+    window.addEventListener("click", checkClick)
+  })
+}
+
+function search(list, keys, search_key){
+    var options = {
+        shouldSort: true,
+        threshold: 0.4,
+        location: 0,
+        distance: 100,
+        maxPatternLength: 32,
+        minMatchCharLength: 1,
+        keys: keys
+      };
+      var fuse = new Fuse(list, options);
+      var result = fuse.search(search_key);
+      var search = document.getElementById("search-item-ul")
+      
+      search.innerHTML = "";
+
+      if (result.length == 0) {
+        search.innerHTML+="<li> No Result Found </li>";
+      } else {
+        result.forEach(function(item){
+          search.innerHTML+="<li>"+item.link+"</li>";
+        });
+      }
+}
