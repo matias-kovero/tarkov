a8e6c88ed906279329f63685506b8c3068fbd5e6
MrBoolean <bryandesigning@gmail.com>
2020-02-23 01:51:51 -0500

    Examples

---
+++ examples/market-buy-item.js
@@ -0,0 +1,32 @@
+const { Tarkov } = require('tarkov');
+
+// We can either provide a pre-generated hwid, or not pass it to Tarkov() for a randomly generated one
+const hwid = 'awesome-sick-hwid';
+const t = new Tarkov(hwid);
+
+// Run everything in an async function
+(async () => {
+
+  // Login to tarkov, optionally also pass 2 factor code as a third param
+  t.login('email@email.com', 'password');
+
+  // Load all profiles we have
+  const profiles = await t.getProfiles();
+
+  // For this example, we're just going to grab the second profile
+  const profile = await t.selectProfile(profiles[1]);
+
+  // Load our i18n translations
+  await t.getI18n('en');
+
+  // Search for an item
+  const search = await t.searchMarket(0, 50, {
+    sortType: 5,
+    sortDirection: 0,
+    currency: 0, // All currencies
+    handbookId: '5c94bbff86f7747ee735c08f', // Labs Access Keycard
+  });
+
+  // Buy the first offer (lowest price);
+  search.offers[0].buyWithRoubles(1);
+})();
